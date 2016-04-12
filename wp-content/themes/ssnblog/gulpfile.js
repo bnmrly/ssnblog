7dfb4a48e2cc602b885e2e43f6a6e53780e25ef3
bnmrly <benjaminjohnmarley@gmail.com>
2016-04-12 11:59:16 +0100

    Added automated Sass and browser refresh with Gulp

---
+++ wp-content/themes/ssnblog/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('sass', function() {
+  return gulp.src('sass/style.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('.'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('sass/**/*.scss', ['sass']);
+  // Other watchers
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    proxy: "localhost/ssnblog"
+  });
+});
