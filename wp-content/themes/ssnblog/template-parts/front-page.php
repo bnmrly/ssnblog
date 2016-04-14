621644bae406691a8b958dee50ab32359e06de07
bnmrly <benjaminjohnmarley@gmail.com>
2016-04-14 14:34:40 +0100

    Added the three divs to front-page.php

--- wp-content/themes/ssnblog/template-parts/front-page.php
+++ wp-content/themes/ssnblog/template-parts/front-page.php
@@ -11,6 +11,22 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+<!-- Code below copied from https://codex.wordpress.org/The_Loop
+
+			<?php
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post();
+					//
+					// Post Content here
+					//
+				} // end while
+			} // end if
+			?>
+
+		-->
+
+
 			<?php
 			while ( have_posts() ) : the_post();
 
