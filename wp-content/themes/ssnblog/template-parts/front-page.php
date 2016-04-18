3d925a42ed71af8b58ca2dcf584f25b3463d7090
bnmrly <benjaminjohnmarley@gmail.com>
2016-04-18 10:44:06 +0100

    Reverted changes and commented out broken code

--- wp-content/themes/ssnblog/template-parts/front-page.php
+++ wp-content/themes/ssnblog/template-parts/front-page.php
@@ -11,26 +11,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-<!-- Code below copied from https://codex.wordpress.org/The_Loop -->
-
-<div id="latest-3-news-posts">
-	latest 3 'news' posts
-<?php //query_posts('category_name=news&showposts=3');
-	//if ( have_posts() ) {
-		//while ( have_posts() ) :
-			  //the_title(); the_permalink();
-				//echo '<h1><a href="'. get_the_permalink() .'">'. get_the_title() ."</a></h1>";
-		//endwhile;
-	//} // end if
-
-	$latest_blog_posts = new WP_Query( array( 'posts_per_page' => 3 , 'category_name' => 'news') );
-
-if ( $latest_blog_posts->have_posts() ) : while ( $latest_blog_posts->have_posts() ) : $latest_blog_posts->the_title();
-    // Loop output goes here
-endwhile; endif;
-	?>
-</div>
-
 			<?php
 			while ( have_posts() ) : the_post();
 
@@ -47,7 +27,9 @@ endwhile; endif;
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-
+	<div id="latest-3-news-posts">
+		latest 3 'news' posts
+	</div>
 
 	<div id="latest-3-stories-posts">
 		latest 3 'stories' posts
