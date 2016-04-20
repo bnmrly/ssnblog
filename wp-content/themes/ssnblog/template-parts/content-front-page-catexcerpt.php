642d51d3a0782eee70d29c2e9f69fb80476ee90d
bnmrly <benjaminjohnmarley@gmail.com>
2016-04-20 10:25:16 +0100

    Changed order of thumbnail in front page post excerpts div

--- wp-content/themes/ssnblog/template-parts/content-front-page-catexcerpt.php
+++ wp-content/themes/ssnblog/template-parts/content-front-page-catexcerpt.php
@@ -14,8 +14,9 @@
 	<header class="entry-header">
 		<?php
 				the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
-				the_excerpt();
 				the_post_thumbnail( 'thumbnail');
+				the_excerpt();
+
 		?>
 	</header><!-- .entry-header -->
 </article>
