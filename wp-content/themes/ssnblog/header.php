c0ea89f08372a60206aa4ceaf2908445c255ad03
bnmrly <benjaminjohnmarley@gmail.com>
2016-04-26 12:01:25 +0100

    Added <body class="page-home" style="touch-action: manipulation;"> following main site

--- wp-content/themes/ssnblog/header.php
+++ wp-content/themes/ssnblog/header.php
@@ -31,7 +31,7 @@
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body body class="page-home" style="touch-action: manipulation;" <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ssnblog' ); ?></a>
 
