028b926716b8ab52f5a9fc6b990552a827b76316
bnmrly <benjaminjohnmarley@gmail.com>
2016-04-26 10:10:45 +0100

    Added new classes for front-page, padding and margins - .cat-excerpt-entry-header and .news-latest-entry-header

--- wp-content/themes/ssnblog/template-parts/content-front-page-catexcerpt.php
+++ wp-content/themes/ssnblog/template-parts/content-front-page-catexcerpt.php
@@ -11,9 +11,9 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header">
+	<header class="entry-header cat-excerpt-entry-header">
 		<?php
-				the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+				the_title( '<h3 class="entry-title cat-excerpt-entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 				the_post_thumbnail( 'thumbnail');
 				the_excerpt();
 
--- wp-content/themes/ssnblog/template-parts/content-front-page-news.php
+++ wp-content/themes/ssnblog/template-parts/content-front-page-news.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header">
+	<header class="entry-header news-latest-entry-header">
 		<?php
 			if ( is_single() ) {
 				the_title( '<h3 class="entry-title">', '</h3>' );
