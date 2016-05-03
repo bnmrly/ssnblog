96572faeb026d208dd814f38c4a64b6cf80c4cd8
bnmrly <benjaminjohnmarley@gmail.com>
2016-05-03 14:11:35 +0100

    Added image-banner-container to display header image

--- wp-content/themes/ssnblog/header.php
+++ wp-content/themes/ssnblog/header.php
@@ -75,4 +75,9 @@
 
 </div> <!-- #container-header -->
 
+  <div class="image-banner-container">
+    <h1 class="h1 block__header">BANNER IMAGE HERE</h1>
+  </div>
+
+
 	<div id="content" class="site-content">
